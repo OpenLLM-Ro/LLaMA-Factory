db48f35691e46be048e593edefdf2a813970bfd8
MihaiMasala <mihaimasala@gmail.com>
2025-07-15 17:44:21 +0300

    Fix broken merge

--- src/llamafactory/webui/components/__init__.py
+++ src/llamafactory/webui/components/__init__.py
@@ -15,10 +15,7 @@
 from .chatbot import create_chat_box
 from .eval import create_eval_tab
 from .export import create_export_tab
-<<<<<<< HEAD
-=======
 from .footer import create_footer
->>>>>>> upstream/main
 from .infer import create_infer_tab
 from .top import create_top
 from .train import create_train_tab
@@ -28,10 +25,7 @@ __all__ = [
     "create_chat_box",
     "create_eval_tab",
     "create_export_tab",
-<<<<<<< HEAD
-=======
     "create_footer",
->>>>>>> upstream/main
     "create_infer_tab",
     "create_top",
     "create_train_tab",
